c2ede67f3adb4c0a4c2474ab6e166b6080b8fcba
André Ottosen <andre.ottosen@no.experis.com>
2020-09-21 21:32:59 +0200

    Implemented top-bar.component and start-page-component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
+import { StartPageComponent } from './components/start-page/start-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopBarComponent,
+    StartPageComponent
   ],
   imports: [
     BrowserModule,
