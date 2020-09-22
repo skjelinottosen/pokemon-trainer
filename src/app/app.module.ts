ab01b60ef9fe9dda68cfa7b7b69de60ccf6bf39d
André Ottosen <andre.ottosen@no.experis.com>
2020-09-22 10:40:01 +0200

    Added footer-bar.component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './components/top-bar/top-bar.component';
 import { StartPageComponent } from './components/start-page/start-page.component';
+import { FooterBarComponent } from './components/footer-bar/footer-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TopBarComponent,
-    StartPageComponent
+    StartPageComponent,
+    FooterBarComponent,
+
   ],
   imports: [
     BrowserModule,
